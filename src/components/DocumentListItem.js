9232143c334ddb0838795ae52157ad3bf5935d3b
ilya-khamula <ilya.khamula@gmail.com>
2021-11-18 11:55:01 +0200

    upd project

--- src/components/DocumentListItem.js
+++ src/components/DocumentListItem.js
@@ -1,10 +1,11 @@
 import Button from "./Button";
+import icon from "../icons/document-list-icon.svg";
 
 function DocumentListItem(props) {
   return (
     <li className="doclist__item">
       <div className="doclist__name-block">
-        <img className="doclist__icon" src="icons/document-list-icon.svg" alt={props.name}/>
+        <img className="doclist__icon" src={icon} alt={props.name}/>
         <div className="doclist__text-block">
           <h3 className="doclist__name">{props.name}</h3>
           <p className="doclist__edited">Last Edited: {props.editedDate}</p>
